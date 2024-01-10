225e9ca73afb16b14a96c08b540565e5a9f05cb1
DimperioB <DimperioB@duvalschools.org>
2024-01-10 12:48:48 -0500

    move config to global load capable

--- src/App/Adapters/Config.fs
+++ src/App/Adapters/Config.fs
@@ -13,10 +13,23 @@ type ConfigType<'t> =
     | Auth of 't
     | Demo
 
-let authConfig = {
-    AppGuid= ""
-    AppAuth= ""
-    ApiScope= ""
-    ApiDomainHint= ""
-    ApiBase= ""
-}
+let private authStorage: Core.LocalStorage.IAccessor<AuthArgs> =
+    Core.LocalStorage.StorageAccess<_>("authConfig")
+
+let authConfig =
+    let value = // value
+        {
+            AppGuid = null
+            AppAuth = null
+            ApiScope = null
+            ApiDomainHint = null
+            ApiBase = null
+        }
+
+    match authStorage.TryGetValue() with
+    | None ->
+        Core.toGlobalWindow "authConfig" value
+        None
+    | Some v -> Some v
+
+    |> Option.defaultValue value
